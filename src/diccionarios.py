17f7cab8d54751685a04b731d0f7c64992f2fcd6
6abriel43 <gabrielguitian202@gmail.com>
2026-04-07 21:30:52 -0300

    Termino funciones del apartado diccionarios en src

--- src/diccionarios.py
+++ src/diccionarios.py
@@ -9,8 +9,11 @@ def contar_palabras(texto: str) -> dict:
     Ejemplo: contar_palabras("hola mundo hola") -> {"hola": 2, "mundo": 1}
     Las palabras deben ser comparadas en minúsculas.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    palabras = texto.lower().split()
+    frecuencia = {}
+    for palabra in palabras:
+        frecuencia[palabra] = frecuencia.get(palabra, 0) + 1
+    return frecuencia
 
 
 def invertir_diccionario(d: dict) -> dict:
@@ -18,16 +21,19 @@ def invertir_diccionario(d: dict) -> dict:
     Retorna un nuevo diccionario con claves y valores intercambiados.
     Ejemplo: invertir_diccionario({"a": 1}) -> {1: "a"}
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    nuevo = {}
+    for clave, valor in d.items():
+        nuevo[valor] = clave
+    return nuevo   
 
 
 def merge_diccionarios(d1: dict, d2: dict) -> dict:
     """
     Combina dos diccionarios. Si hay claves repetidas, prevalece d2.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    resultado = d1.copy()
+    resultado.update(d2)
+    return resultado
 
 
 def filtrar_por_valor(d: dict, minimo: int) -> dict:
@@ -35,5 +41,8 @@ def filtrar_por_valor(d: dict, minimo: int) -> dict:
     Retorna un nuevo diccionario con solo los pares
     cuyo valor sea >= minimo.
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    diccionarioFiltrado = {}
+    for clave, valor in d.items():
+        if valor >= minimo:
+            diccionarioFiltrado[clave] = valor
+    return diccionarioFiltrado
